dfa7d09b32046d45d1fbbec1db630b592b0ee52a
Jeferson <jefeleal363@gmail.com>
2024-09-05 12:46:06 -0300

    feat: javascript

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao:
+        'Rebeca Andrade é a maior ginasta da história do Brasil. Nascida em Guarulhos, ela conquistou o país com sua força, determinação e talento. Com seis medalhas olímpicas, incluindo dois ouros, Rebeca se tornou a primeira brasileira bicampeã olímpica em uma prova ndividual.',
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    },
+    {
+        titulo: "Rayssa Leal",
+        descricao:
+        'Rayssa Leal é uma skatista brasileira, conhecida como "Fada". Aos 13 anos, conquistou a prata nos Jogos Olímpicos de Tóquio 2020, tornando-se a mais jovem medalhista olímpica do Brasil.',
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao:
+        'Beatriz Souza é uma judoca brasileira. Conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024, tornando-se um dos grandes nomes do judô brasileiro.',
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    },
+];
--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+    let resultados = "";
+
+    for (let dado of dados) {
+      resultados += `
+    <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">
+            ${dado.descricao}
+          </p>
+          <a href="${dado.link}" target="_blank"
+            >Mais informações</a
+          >
+        </div>
+`;
+    }
+
+    section.innerHTML = resultados;
+}
+
+
+
+
